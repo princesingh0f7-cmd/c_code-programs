cc69e74036367a81e00b36de8be2201523dd9cfc
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-22 20:35:56 +0530

    Area of triangle

---
+++ 08_problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// area of traingle
+int main(){
+int breath,height;
+printf("ENTER YOUR BREATH OF TRIANGLE:");
+scanf("%d",&breath);
+printf("ENTER YOUR HEIGHT OF TRIANGLE:");
+scanf("%d",&height);
+printf("AREA OF TRIANGLE: %f",0.5*breath*height);
+    return 0;
+}
