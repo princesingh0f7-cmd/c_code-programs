51eaa1f23b3aded2f6bd2087c07c04118b0506ac
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-22 20:22:35 +0530

    Ternary Operator

---
+++ 04_problem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+char gender;
+printf("Enter You Character of Gender (M/F):");
+scanf("%c",&gender);
+gender=='M'? printf("Male") : printf("Female");
+    return 0;
+}
