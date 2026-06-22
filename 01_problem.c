26aae1763ba91ec82d018a4265996114ea43945a
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-22 20:15:02 +0530

    Sum of two number

---
+++ 01_problem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+int a,b;
+printf("ENTER YOUR FIRST NUMBER:2");
+scanf("%d",&a);
+printf("ENTER YOUR SECOND NUMBER:");
+scanf("%d",&b);
+printf("SUM OF TWO NUMBER= %d",a+b);
+    return 0;
+}
