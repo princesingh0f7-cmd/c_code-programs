ac85f629550353a2583aae2bc05dd903e2630eaf
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-22 20:23:58 +0530

    Switch Operator (day_example)

---
+++ 05_problem.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+int day;
+printf("Enter Your Day no(1-7):");
+scanf("%d",&day);
+switch (day)
+{
+case 1: 
+printf ("Today is Monday");
+break;
+case 2:
+printf ("Today is Tuesday");
+break;
+case 3: 
+printf ("Today is Wednesday");
+break;
+case 4: 
+printf ("Today is Thursday");
+break;
+case 5: 
+printf ("Today is Friday");
+break;
+case 6: 
+printf ("Today is Saturday");
+break;
+case 7: 
+printf ("Today is Sunday");
+break;
+}
+    return 0;
+}
