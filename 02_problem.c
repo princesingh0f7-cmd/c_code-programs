e2903388856511811d998a9372a810f3b0debf59
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-22 20:19:12 +0530

    Explict Conversion

---
+++ 02_problem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int a=(int)1.999999;
+printf("%d",a);
+int number;
+printf("\nENTER YOUR NUMBER:");
+scanf("%d",&number);
+printf("Your Number is %d",number);
+printf("\nConvert into float is %f",(float) number);
+    return 0;
+}
