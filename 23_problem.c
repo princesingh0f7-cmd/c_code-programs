1491f61a031029cd14b21a514293a32812733168
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-23 22:45:55 +0530

    Sum of Digit

---
+++ 23_problem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// SUM OF DIGIT OF NUMBER
+int main(){
+int num;
+printf("ENTER NUMBER:");
+scanf("%d",&num);
+int orignal=num;
+int sum =0;
+for(int i=1;num>0;i++){
+sum=sum+(num%10);
+num=num/10;
+}
+printf("sum of digit %d is %d",orignal,sum);
+    return 0;
+}
