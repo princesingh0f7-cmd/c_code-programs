52a676e7dae278427a4d19faac8fd277284259f9
princesingh0f7-cmd <princesingh0f4@gmail.com>
2026-06-24 10:28:34 +0530

    fabiconni series

--- 23_problem.c
+++ 23_problem.c
@@ -8,7 +8,7 @@ int orignal=num;
 int sum =0;
 for(int i=1;num>0;i++){
 sum=sum+(num%10);
-num=num/10;
+num=num/10; //123/10=12.3 dicard decimal
 }
 printf("sum of digit %d is %d",orignal,sum);
     return 0;
